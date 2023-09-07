53f8b29d38f84c95c452671ccf3e5a445cc42151
hana-ri <hm.rizal1933@gmail.com>
2023-09-07 15:51:35 +0700

    update (changed): changed name (Thursday, 09072023)

--- lang/en/customequiz.php
+++ lang/en/customequiz.php
@@ -1,7 +1,7 @@
 <?php
 
-$string['modulename']                      = 'Custome Quiz Tekkom';
-$string['modulenameplural']                = 'Custome Quiz Tekkom';
-$string['pluginadministration']            = 'Custome Quiz Tekkom Administration';
-$string['pluginname']                      = 'Custome Quiz Tekkom';
+$string['modulename']                      = 'Cryptography Plugin for Moodle Secure Quiz';
+$string['modulenameplural']                = 'Cryptography Plugin for Moodle Secure Quiz';
+$string['pluginadministration']            = 'Cryptography Plugin for Moodle Secure Quiz Administration';
+$string['pluginname']                      = 'Cryptography Plugin for Moodle Secure Quiz';
 $string['customequizname']                 = 'Name';
